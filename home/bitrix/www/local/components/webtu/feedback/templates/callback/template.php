9cc6b49f312a1b25b0bd457438d5ad87706619b6
andrew1762 <a.vanzha@tsbnk.ru>
2023-11-23 17:26:17 +0300

    tsb-48 переделал заполнение массива dataLayer в форме /feedback/templates/callback/

--- home/bitrix/www/local/components/webtu/feedback/templates/callback/template.php
+++ home/bitrix/www/local/components/webtu/feedback/templates/callback/template.php
@@ -159,80 +159,6 @@ while($arMess = $rs_mess->GetNext()) { // нахожу ID почтового ш�
            clearFields ();
        }
 
-       $('.feedback_form .button').click(function () {
-           $(".alert").remove();
-       });
-
-       $('.agreement input[required]').change(function () {
-           if ( $(this).is(':checked') ) {
-               $(this).closest('.agreement').css('box-shadow', '');
-           } else {
-               $(this).closest('.agreement').css('box-shadow', '0 0 2px 1px red');
-           }
-       });
-
-       // https://osipenkov.ru/tracking-fileds-yandex-metrika-gtm/
-       // https://blog.targeting.school/kakie-byvayut-tseli-v-ya-metrike-i-kak-rabotaet-novaya-tsel-otpravka-formy/
-       // https://www.yandex.ru/video/preview/17446571467160561628
-       function yandexMetrikaForm() {
-           //yaCounter49389685
-           //yaCounter315345643.reachGoal('applicationForm'); // ошика
-           //ym(315345643, 'reachGoal', 'applicationForm');
-
-           let formFields = {
-               'Отправка формы':
-                   {
-                       //'Имя получателя': {{Поле JS - Имя получателя}},
-                       'Имя получателя': 'Имя получателя',
-                       //'Email получателя': {{Поле JS - Email получателя}},
-                       'Email получателя': 'Email получателя',
-                       //'Ваше имя': {{Поле JS - Ваше имя}},
-                       'Ваше имя': 'Ваше имя',
-                       //'Ваш Email': {{Поле JS - Ваш email}},
-                       'Ваш Email': 'Поле JS - Ваш email',
-                       //'Тема подарочного сертификата': {{Поле JS - Тема подарочного сертификат}},
-                       'Тема подарочного сертификата': 'Поле JS - Тема подарочного сертификат',
-                       //'Сообщение': {{Поле JS - Сообщение}},
-                       'Сообщение': 'Поле JS - Сообщение',
-                       //'Сумма': {{Поле JS - Сумма}},
-                       'Сумма': 'Поле JS - Сумма',
-                   }
-           };
-           //ym(955, 'reachGoal', 'applicationForm', formFields);
-
-           let entry = {
-               'PRODUCT_ID': 0,
-               'NAME': 'form',
-               'PRICE': 11,
-               'DETAIL_PAGE_URL': '<?= $_SERVER['REQUEST_URI'] ?>',
-               'QUANTITY': 1,
-               'XML_ID': 'xml'
-           };
-           let postTemplateID = <?= $postTemplateID; ?>;
-           if(postTemplateID) {
-               entry.PRODUCT_ID = postTemplateID; // ID почтового шаблона
-           }
-           //console.log('postTemplateID');
-           //console.log(postTemplateID);
-           let pos = 1;
-           let ar_product = [];
-           ar_product.push(
-               {
-                   "id": entry.PRODUCT_ID,
-                   "name": entry.NAME,
-                   "price": entry.PRICE,
-                   "category": entry.DETAIL_PAGE_URL,
-                   "quantity": entry.QUANTITY,
-                   "position": pos++,
-                   "xml": entry.XML_ID,
-               },
-           );
-           makeDataLayer(1, ar_product);
-           //console.log(window.dataLayer);
-
-           return true;
-       }
-
        function makeDataLayer(id, ar_product) {
            window.dataLayer.push({
                //local_dataLayer.push({
@@ -247,5 +173,46 @@ while($arMess = $rs_mess->GetNext()) { // нахожу ID почтового ш�
                }
            });
        }
+
+       let pos = 1;
+       $('.feedback_form .button').click(function () {
+           let entry = {
+               'PRODUCT_ID': 0,
+               'NAME': 'form',
+               'PRICE': 1,
+               'DETAIL_PAGE_URL': '<?= $_SERVER['REQUEST_URI'] ?>',
+               'QUANTITY': 1,
+               'XML_ID': 'xml'
+           };
+           let ar_product = [];
+           let postTemplateID = <?= $postTemplateID; ?>;
+           if(postTemplateID) {
+               entry.PRODUCT_ID = postTemplateID; // ID почтового шаблона
+           }
+           ar_product.push(
+               {
+                   "id": entry.PRODUCT_ID,
+                   "name": entry.NAME,
+                   "price": entry.PRICE,
+                   "category": entry.DETAIL_PAGE_URL,
+                   "quantity": entry.QUANTITY,
+                   "position": 1,
+                   "xml": entry.XML_ID,
+               },
+           );
+           makeDataLayer(pos++, ar_product);
+           console.log(window.dataLayer);
+           //yandexMetrikaForm();
+
+           $(".alert").remove();
+       });
+
+       $('.agreement input[required]').change(function () {
+           if ( $(this).is(':checked') ) {
+               $(this).closest('.agreement').css('box-shadow', '');
+           } else {
+               $(this).closest('.agreement').css('box-shadow', '0 0 2px 1px red');
+           }
+       });
    });
 </script>
